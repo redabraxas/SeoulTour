c8b71adc9ccfe54843b4acb2ccdbfd2d3227b0b8
kimboha <asabe1201@gmail.com>
2015-05-19 04:48:18 +0900

    ProgressActivity.java

--- app/src/main/java/com/chocoroll/seoultour/Main/ProgressActivity.java
+++ app/src/main/java/com/chocoroll/seoultour/Main/ProgressActivity.java
@@ -4,18 +4,23 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.telephony.TelephonyManager;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.chocoroll.seoultour.Adapter.ProgressAdapter;
+import com.chocoroll.seoultour.Model.District;
+import com.chocoroll.seoultour.Model.Item;
 import com.chocoroll.seoultour.R;
 import com.chocoroll.seoultour.Retrofit.Retrofit;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-
-import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -24,12 +29,13 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
+
 public class ProgressActivity extends ActionBarActivity {
-
     ProgressDialog dialog;
-
-    // 여기 어레이리스트
-//    ArrayList<> arrayList = new ArrayList<>;
+    ArrayList<Item> arrayList;
+    ProgressAdapter adepter;
+    ArrayList<District> distList;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +43,10 @@ public class ProgressActivity extends ActionBarActivity {
         setContentView(R.layout.activity_progress);
 
         getStampList();
+
+        Intent intent = getIntent();
+        distList = intent.getParcelableArrayListExtra("districtList");
     }
-
-
 
     void getStampList(){
 
@@ -56,16 +63,14 @@ public class ProgressActivity extends ActionBarActivity {
         final JsonObject info = new JsonObject();
         info.addProperty("phoneID",phoneID);
 
-
         new Thread(new Runnable() {
             public void run() {
                 try {
-
                     RestAdapter restAdapter = new RestAdapter.Builder()
-                            .setEndpoint(Retrofit.ROOT)  //call your base url
-                            .build();
-                    Retrofit sendreport = restAdapter.create(Retrofit.class); //this is how retrofit create your api
-                    sendreport.getStampList(info, new Callback<JsonArray>() {
+                                .setEndpoint(Retrofit.ROOT)  //call your base url
+                                .build();
+                        Retrofit sendreport = restAdapter.create(Retrofit.class); //this is how retrofit create your api
+                        sendreport.getStampList(info, new Callback<JsonArray>() {
 
                         @Override
                         public void success(JsonArray jsonElements, Response response) {
@@ -74,36 +79,36 @@ public class ProgressActivity extends ActionBarActivity {
 
                             for(int i=0; i<jsonElements.size(); i++) {
                                 JsonObject deal = (JsonObject) jsonElements.get(i);
-                                String code = (deal.get("districtCode")).getAsString();
-                                String name = (deal.get("districtName")).getAsString();
-
                                 int count = (deal.get("count")).getAsInt();
+                                String code = (deal.get("districtCode")).getAsString();
+                                String name = distList.get(Integer.valueOf(code)-1).getName();
 
-//                                arrayList.add(new Deal());
-
+                                //String str = "mydata : " + count + " " + code + " " + name;
+                                //Log.e("mydebug", str);
+                                arrayList.add(new Item(code, name, count));
                             }
-
                             // 전체는  districtCode =0   / districtName= 전체    이렇게 보내겟음.
-
+                            init();
+                            totalProgress();
 
                         }
 
                         @Override
                         public void failure(RetrofitError retrofitError) {
                             dialog.dismiss();
+
                             AlertDialog.Builder builder = new AlertDialog.Builder(ProgressActivity.this);
                             builder.setTitle("네트워크가 불안정합니다.")        // 제목 설정
                                     .setMessage("네트워크를 확인해주세요")        // 메세지 설정
                                     .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
                                     .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                        // 확인 버튼 클릭시 설정
+                                        //확인 버튼 클릭시 설정
                                         public void onClick(DialogInterface dialog, int whichButton) {
                                         }
                                     });
 
                             AlertDialog dialog = builder.create();    // 알림창 객체 생성
                             dialog.show();    // 알림창 띄우기
-
                         }
                     });
                 }
@@ -115,5 +120,34 @@ public class ProgressActivity extends ActionBarActivity {
 
     }
 
+    void init(){
+        arrayList = new ArrayList<Item>();
+        // test
+        arrayList.add( new Item("15", "seocho", 8));        // code, name, cnt
 
+        adepter = new ProgressAdapter(getApplicationContext(), R.layout.procress_row, arrayList);
+
+        // 리스트 표현하기
+        listView = (ListView)findViewById(android.R.id.list);
+        listView.setAdapter(adepter);
+
+        // 리스트 디자인
+        listView.setDivider(new ColorDrawable(Color.BLUE)); // 리스트간 경계선
+        listView.setDividerHeight(5);
+    }
+
+    void totalProgress(){
+        int stampSum = 0, totalSum = 0 ;
+        //totalSum =
+        for(int i=0; i<arrayList.size(); i++)
+        {
+            stampSum += Integer.valueOf(arrayList.get(i).getCnt());      // 도장 찍힌 개수 카운트
+        }
+
+        ProgressBar totalBar = (ProgressBar)findViewById(R.id.totalBar);
+        totalBar.setProgress((int)(stampSum / totalSum * 100));
+
+        TextView tx = (TextView)findViewById(R.id.totalBarText);
+        tx.setText(stampSum + " / " + totalSum);
+    }
 }
