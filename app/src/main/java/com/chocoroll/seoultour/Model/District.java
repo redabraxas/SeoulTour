cce9d0ec83e88247ae8db7bbc4f904d38f71a8ef
redabraxas <udhee94@nate.com>
2015-05-19 03:41:35 +0900

    ddd

--- app/src/main/java/com/chocoroll/seoultour/Model/District.java
+++ app/src/main/java/com/chocoroll/seoultour/Model/District.java
@@ -1,12 +1,19 @@
 package com.chocoroll.seoultour.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by RA on 2015-05-13.
  */
-public class District {
+public class District implements Parcelable {
     private String name;
     private   double mapx, mapy;
     private  String code;
+
+    public District(Parcel in) {
+        readFromParcel(in);
+    }
 
     public District(String code, String name, double mapx, double mapy){
         this.code = code;
@@ -30,4 +37,37 @@ public class District {
     public String getCode() {
         return code;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+        parcel.writeString(name);
+        parcel.writeDouble(mapx);
+        parcel.writeDouble(mapy);
+        parcel.writeString(code);
+
+    }
+
+    private void readFromParcel(Parcel in){
+        name = in.readString();
+        mapx=in.readDouble();
+        mapy = in.readDouble();
+        code = in.readString();
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public District createFromParcel(Parcel in) {
+            return new District(in);
+        }
+
+        public District[] newArray(int size) {
+            return new District[size];
+        }
+    };
+
 }
